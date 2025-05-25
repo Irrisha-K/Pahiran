fe9122201ff08f1079329c5aceb680e5a038efc8
Irrisha Kayastha <irrishakayastha@gmail.com>
2025-05-25 13:11:57 +0545

    Data Fetched from Backend Tops Updated

--- src/Products/pages/TopsPage.jsx
+++ src/Products/pages/TopsPage.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductsPage.css";
 import NewArrivalsList from "../../NewArrivals/Components/NewArrivalsList";
+import Card from "../../shared/components/UIElements/Card";
+import ProductList from "../../Products/components/ProductsList";
 
 const products = [
   {
@@ -30,11 +32,66 @@ const products = [
 ];
 
 export default function TopsPage() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null); // ⬅️ error state
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("http://localhost:5001/api/products/tops");
+        if (!res.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await res.json();
+        setProducts(data);
+      } catch (err) {
+        setError("Failed to load products. Please check your network.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="no-product-container">
+        <Card className="no-product">
+          <p className="no-product-text">Loading products...</p>
+        </Card>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="no-product-container">
+        <Card className="no-product">
+          <p className="no-product-text">{error}</p>
+        </Card>
+      </div>
+    );
+  }
+
+  if (products.length === 0) {
+    return (
+      <div className="no-product-container">
+        <Card className="no-product">
+          <p className="no-product-text">
+            No Items Found! Please Try Again Later!
+          </p>
+        </Card>
+      </div>
+    );
+  }
+
   return (
     <>
       {/* <ImageSlider /> */}
 
-      <NewArrivalsList items={products} />
+      <ProductList items={products} />
     </>
   );
 }
